36da9bdb7829bf0521e45210219c05f8b25cd95a
MINAMI-VR <41174028+MINAMI-VR@users.noreply.github.com>
2018-07-30 19:11:53 +0900

    Initial commit

---
+++ brainfuck.py
@@ -0,0 +1,21 @@
+import sys
+
+list_ = [0] * 1000
+while 1:
+    command = input()
+    if command == "reset":
+        list_ = [0] * 1000
+    command_list = list(command)
+    pointer = 0
+    for i in command_list:
+        if i == ">" and pointer < 1000:
+            pointer = pointer + 1
+        if i == "<" and pointer > 0:
+            pointer = pointer - 1
+        if i == "+":
+            list_[pointer] = list_[pointer] + 1
+        if i == "-":
+            list_[pointer] = list_[pointer] - 1
+        if i == ".":
+            sys.stdout.write(chr(list_[pointer]))
+    print()
