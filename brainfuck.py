c778f692a4397a8f65bdee8517a5938ba043aaab
MINAMI-VR <41174028+MINAMI-VR@users.noreply.github.com>
2018-07-31 22:20:25 +0900

    2nd commit

--- brainfuck.py
+++ brainfuck.py
@@ -1,27 +1,47 @@
 import sys
 
-list_ = [0] * 1000
+length = 1000
+list_ = [0] * length
 
 while 1:
     command = input()
     if command == "reset":
-        list_ = [0] * 1000
+        list_ = [0] * length
+        pointer = 0
     if command == "exit":
         break
 
     command_list = list(command)
     pointer = 0
-    for i in command_list:
-        if i == ">" and pointer < 1000:
+    i = 0
+    while i < len(command_list):
+        if command_list[i] == ">" and pointer < 1000:
             pointer = pointer + 1
-        if i == "<" and pointer > 0:
+        if command_list[i] == "<" and pointer > 0:
             pointer = pointer - 1
-        if i == "+":
+        if command_list[i] == "+":
             list_[pointer] = list_[pointer] + 1
-        if i == "-":
+        if command_list[i] == "-":
             list_[pointer] = list_[pointer] - 1
-        if i == ".":
+        if command_list[i] == ".":
             sys.stdout.write(chr(list_[pointer]))
-        if i == ",":
+        if command_list[i] == ",":
             list_[pointer] = ord(input("> "))
+        if command_list[i] == "[" and list_[pointer] == 0:
+            j = i + 1
+            while j < length:
+                if command_list[j] == "]":
+                    i = j
+                    k = 1
+                    break
+                j = j + 1
+        if command_list[i] == "]" and k == 0:
+            j = i - 1
+            while j >= 0:
+                if command_list[j] == "[":
+                    i = j - 1
+                    break
+                j = j - 1
+        i = i + 1
+        k = 0
     print()
